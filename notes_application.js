d9ed695d9e5b9cbf6e696a54da3f28125ed3f75a
Chineze <nwosuchineze@gmail.com>
2016-05-17 16:10:12 +0100

    add search function

--- notes_application.js
+++ notes_application.js
@@ -5,14 +5,24 @@ function NotesApplication(author) {
 		this.notes.push(note_content);
 	}
 	this.listNotes = function() {
-		var notelist = "";
+		var noteList = "";
 		for(var i = 0; i < notes.length; i++){
-			notelist += "Note ID: " + i + "\n" + this.notes[i] + "\n\nBy Author " + this.author + "\n\n";
+			noteList += "Note ID: " + i + "\n" + this.notes[i] + "\n\nBy Author " + this.author + "\n\n";
 		}
-		return notelist;
+		return noteList;
 	}
 	this.get = function(note_id) {
 		return this.notes[note_id];
+	}
+	this.search = function(search_text) {
+		var resultList = "Showing results for search ‘<" + search_text + ">’\n\n";
+		for(var i = 0; i < notes.length; i++){
+			if(notes[i].includes(search_text))
+			{
+				resultList += "Note ID: " + i + "\n" + this.notes[i] + "\n\nBy Author " + this.author + "\n\n";
+			}
+		}
+		return resultList;
 	}
 	
 }
